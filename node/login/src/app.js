43db504874eed9a6992f7ab9e9a2a45e79de7c3d
RuquiaAlam <114290800+RuquiaAlam@users.noreply.github.com>
2023-05-05 02:58:23 +0530

    up

--- node/login/src/app.js
+++ node/login/src/app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 4000;
 const cors = require("cors");
 app.use(cors());
 
-const AuthController = require("./controller/authController");
+const AuthController = require("../controller/authController");
 app.use("/api/auth", AuthController);
 
 app.listen(PORT, () => {
